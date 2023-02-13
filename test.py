4aa49dc349c5f46fe5bd0c4afd6a68812629a656
お墓さま <102584033+I-am-a-cool-guy@users.noreply.github.com>
2023-02-13 14:44:06 +0900

    tenga...?

---
+++ test.py
@@ -0,0 +1,23 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix=["!",""],intents=intents)
+bot.remove_command("help")
+
+@bot.event
+async def on_message(message):
+    if message.author.bot:
+        return
+    else:
+        if message.guild:
+            CHANNEL_NAME = "聞き専","雑談"
+            if message.channel.name in CHANNEL_NAME:
+                print(f"{message.author.name} | {message.content}  ----{message.guild.name}メンバー数({message.guild.member_count})")
+                f2 = open('index.html', 'a', encoding='UTF-8')
+                for attachment in message.attachments:
+                    if attachment.url.endswith(("png", "jpg", "jpeg")):
+                        f2.writelines(f"<nav><h1>{message.author.name}<span>{message.created_at}</span></h1><p>{message.content}</p><img src={attachment.url}><nav>\n")
+                        f2.close()
+
+bot.run(Token)
